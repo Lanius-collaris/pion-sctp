61c9d8cc902d7280c11da00c3f99da0df025c8d7
Lanius-collaris <55432068+Lanius-collaris@users.noreply.github.com>
2026-03-02 19:15:03 +0800

    fix tests

--- association_test.go
+++ association_test.go
@@ -6595,7 +6595,7 @@ func TestFastRecoveryExitOnAckedExitPoint(t *testing.T) {
 	assoc.fastRecoverExitPoint = 101
 
 	assoc.lock.Lock()
-	_, _, _, _, _, err := assoc.processSelectiveAck(&chunkSelectiveAck{ //nolint:dogsled
+	_, _, _, _, _, _, err := assoc.processSelectiveAck(&chunkSelectiveAck{ //nolint:dogsled
 		cumulativeTSNAck: 101,
 	})
 	exited := !assoc.inFastRecovery
