1933fedcc9b4b0e7ff42e974861652a38ab3a469
Lanius-collaris <55432068+Lanius-collaris@users.noreply.github.com>
2026-03-10 07:09:59 +0800

    retransmit before sending new data

--- association.go
+++ association.go
@@ -1762,8 +1762,8 @@ func (a *Association) gatherOutbound() ([][]byte, bool) {
 		consumed := false
 
 		rawPackets = a.gatherDataPacketsToRetransmit(rawPackets, &budgetUnits, &consumed)
-		rawPackets = a.gatherOutboundDataAndReconfigPackets(rawPackets, &budgetUnits, &consumed)
 		rawPackets = a.gatherOutboundFastRetransmissionPackets(rawPackets, &budgetUnits, &consumed)
+		rawPackets = a.gatherOutboundDataAndReconfigPackets(rawPackets, &budgetUnits, &consumed)
 
 		// control traffic shouldn't be limited.
 		rawPackets = a.gatherOutboundSackPackets(rawPackets)
